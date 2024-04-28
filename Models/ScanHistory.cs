911d9334c5ecda16026f4a64f4f26d4be5266038
Karoline Aspegren <karolineaspegren@gmail.com>
2024-04-28 19:20:56 +0200

    Removed classes from solution

--- Models/ScanHistory.cs
+++ Models/ScanHistory.cs
@@ -4,8 +4,6 @@ namespace PetScanner.Models;
 
 public class ScanHistory
 {
-    //To be used with wl-134 module
-    public List<ScanResponse> HistoryList { get; set; }
 
     public List<TimeResponse> TimeList { get; set; }
 }
